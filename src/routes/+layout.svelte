763ddb0538c0d5ef02636bbc5cb17d6ef7fffb2b
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2023-12-12 14:29:21 +0100

    feat: navbar and redirect to signin page when trying to navigate to protected page while logged out

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,35 @@
 <script>
     import "../app.pcss";
+    import { Button } from "$lib/components/ui/button/index";
+    import { signIn, signOut } from "@auth/sveltekit/client"
+    import { page } from "$app/stores"
+    import { Avatar, AvatarFallback, AvatarImage } from "$lib/components/ui/avatar/index";
 </script>
 
-<slot />
+<div class="flex flex-col h-screen">
+    <div class="flex justify-between m-2 gap-4">
+        <nav class="flex items-center gap-2">
+            <Button href="/">Home</Button>
+            <Button href="/map">Map</Button>
+            <Button href="/statistics">Statistics</Button>
+        </nav>
+        <div class="flex gap-2 items-center">
+            {#if $page.data.session}
+                <span>Signed in as</span>
+                <Avatar>
+                    <AvatarImage src={$page.data.session.user?.image}
+                                 alt="Profile picture of {$page.data.session.user?.name}"></AvatarImage>
+                    <AvatarFallback>{$page.data.session.user?.name?.split(" ").map(name => name[0]).join("")}</AvatarFallback>
+                </Avatar>
+                <strong>{$page.data.session.user?.name ?? "User"}</strong>
+                <Button on:click={() => signOut()} class="button">Sign out</Button>
+            {:else}
+                <span>You are not signed in</span>
+                <Button on:click={() => signIn("keycloak")}>Sign in via keycloak</Button>
+            {/if}
+        </div>
+    </div>
+    <main class="flex-1">
+        <slot />
+    </main>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,20 @@
 <script lang="ts">
 import { Button } from "$lib/components/ui/button/index";
-import { signIn, signOut } from "@auth/sveltekit/client"
-import { page } from "$app/stores"
-import { Avatar, AvatarFallback, AvatarImage } from "$lib/components/ui/avatar/index";
 import type { ActionData } from "./$types"
 
 export let form: ActionData;
 </script>
 
-<pre>
-    {JSON.stringify($page.data, null, 2)}
-</pre>
-
-<p>
-    {#if $page.data.session}
-        <Avatar>
-            <AvatarImage src={$page.data.session.user?.image} alt="Profile picture of {$page.data.session.user?.name}"></AvatarImage>
-            <AvatarFallback>{$page.data.session.user?.name?.split(" ").map(name => name[0]).join("")}</AvatarFallback>
-        </Avatar>
-        <span class="signedInText">
-      <small>Signed in as</small><br />
-      <strong>{$page.data.session.user?.name ?? "User"}</strong>
-    </span>
-        <Button on:click={() => signOut()} class="button">Sign out</Button>
-    {:else}
-        <span class="notSignedInText">You are not signed in</span>
-        <Button on:click={() => signIn("keycloak")}>Sign in via keycloak</Button>
-    {/if}
-</p>
-
-<form method="POST">
-    <Button type="submit">Hit backend</Button>
-    {#if form?.success}
-        <p>Success</p>
-    {:else}
-        <p>Womp womp</p>
-    {/if}
-</form>
+<div class="m-2">
+    <form method="POST">
+        <Button type="submit">Hit backend</Button>
+        {#if !form}
+            <p>Go on, hit it</p>
+        {:else if form.success}
+            <p>Success</p>
+        {:else}
+            <p>Womp womp</p>
+        {/if}
+    </form>
+</div>
 
