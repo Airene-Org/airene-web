1081d9439524a9f42e2ec8efd2d84825e15730d6
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2023-12-19 15:56:08 +0100

    light and dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,9 @@
     import { signIn, signOut } from "@auth/sveltekit/client"
     import { page } from "$app/stores"
     import { Avatar, AvatarFallback, AvatarImage } from "$lib/components/ui/avatar/index";
+    import { ModeWatcher } from "mode-watcher";
+    import { Sun, Moon } from "radix-icons-svelte";
+    import { toggleMode } from "mode-watcher";
 </script>
 
 <div class="flex flex-col h-screen">
@@ -16,7 +19,7 @@
         <div class="flex gap-2 items-center">
             {#if $page.data.session}
                 <span>Signed in as</span>
-                <Avatar>
+                <Avatar class="h-9 w-9">
                     <AvatarImage src={$page.data.session.user?.image}
                                  alt="Profile picture of {$page.data.session.user?.name}"></AvatarImage>
                     <AvatarFallback>{$page.data.session.user?.name?.split(" ").map(name => name[0]).join("")}</AvatarFallback>
@@ -27,9 +30,19 @@
                 <span>You are not signed in</span>
                 <Button on:click={() => signIn("keycloak")}>Sign in</Button>
             {/if}
+            <Button on:click={toggleMode} variant="outline" size="icon">
+                <Sun
+                        class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+                />
+                <Moon
+                        class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+                />
+                <span class="sr-only">Toggle theme</span>
+            </Button>
         </div>
     </div>
     <main class="flex-1">
+        <ModeWatcher />
         <slot />
     </main>
 </div>
